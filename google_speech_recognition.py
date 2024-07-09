86253e807ca3a179ce6fc49ef2208832e81f8f34
Yaret Berges Calero <yaretejea@gmail.com>
2024-07-09 18:53:16 +0200

    Updating google_speech_recognition.py. Changing listening name from "hey tux" to "alexa" due to the problems of the software to recognize the "hey tux". Setting timeouts to listeners. Trying to open all links into the same tab (still in progress).

--- google_speech_recognition.py
+++ google_speech_recognition.py
@@ -3,43 +3,53 @@ from youtube_music_api_client import youtube_api_query
 from gtts_speech_to_voice import text_to_speech
 from pygame import mixer, init
 
-recognizer = sr.Recognizer()
 youtube_music_patterns = ['pon música de', 'pon la canción', 'pon']
-tux_patters = ["hey tuxt", "hey tux", "itunes"]
+#tux_patters = ["hey tuxt", "itox", "hey tucson", "hey dux", "hey tux", "oye tux", "oye tuxt", "itunes", "hey tu", "oye tucson", "oye dux", "oye lux", "hey jux", "el tux", "oye tuc", "oye tú", "oye"]
+voice_assistant_patters = ["alex", "alexa"]
 init()
 listening_sound = mixer.Sound("audio/mixkit-positive-interface-beep-221.wav")
 error_sound = mixer.Sound("audio/error-8-206492.mp3")
 
 
+for index, name in enumerate(sr.Microphone.list_microphone_names()):
+    print(f"{index} -> {name}")
+    if "HyperX Cloud Flight S: USB Audio" in name:
+        device_audio_index = index
+        break
+
+print(device_audio_index)
+
+
 def record_audio():
-    with sr.Microphone() as source:
+    recognizer = sr.Recognizer()
+    mic = sr.Microphone(device_index=device_audio_index)
+    with mic as source:
         while True:
             try:
-                audio = recognizer.listen(source)
+                audio = recognizer.listen(source, timeout=1, phrase_time_limit=1)
                 query = recognizer.recognize_google(audio, language="es-ES")
                 query = query.lower()
-                if any(tux_patter in query for tux_patter in tux_patters):
-                    listening_sound.play()
-                    audio = recognizer.listen(source)
-                    query = recognizer.recognize_google(audio, language="es-ES")
-                    recognize_speech(query)
-            except sr.UnknownValueError:
+                print(query)
+                try:
+                    if any(voice_assistant_patter in query for voice_assistant_patter in voice_assistant_patters):
+                        listening_sound.play()
+                        audio = recognizer.listen(source, phrase_time_limit=5, timeout=3)
+                        query = recognizer.recognize_google(audio, language="es-ES")
+                        print(query)
+                        recognize_speech(query)
+                except (sr.UnknownValueError, sr.exceptions.WaitTimeoutError):
+                    error_sound.play()
+            except (sr.UnknownValueError, sr.exceptions.WaitTimeoutError):
                 pass
 
 
 def recognize_speech(query):
-    try:
-        if any(youtube_music_pattern in query for youtube_music_pattern in youtube_music_patterns):
-            query = next((query.replace(pattern, "") for pattern in youtube_music_patterns if pattern in query), query)
-            text_to_speech(f"Vale, voy a intentar reproducir {query}")
-            youtube_api_query(query)
-        else:
-            error_sound.play()
-
-    except sr.UnknownValueError:
-        text_to_speech("Lo siento, no te he entendido")
-    except sr.RequestError:
-        text_to_speech("Ha habido un problema mientras se procesaba la solicitud. Por favor, intentelo de nuevo")
+    if any(youtube_music_pattern in query for youtube_music_pattern in youtube_music_patterns):
+        query = next((query.replace(pattern, "") for pattern in youtube_music_patterns if pattern in query), query)
+        text_to_speech(f"Vale, voy a intentar reproducir {query}")
+        youtube_api_query(query)
+    else:
+        error_sound.play()
 
 
 if __name__ == "__main__":
